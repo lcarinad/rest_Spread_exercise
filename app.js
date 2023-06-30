df862a83dd3cba8120dc0f8e390a832ae548ff8f
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-06-30 14:57:44 -0400

    wrote doublesandarr func

--- app.js
+++ app.js
@@ -17,3 +17,11 @@ const findMin = (...args) => Math.min(...args);
 //3️⃣mergeObjects: Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
 //mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
 const mergeObjects = (obj1, obj2) => ({ ...obj1, ...obj2 });
+//4️⃣doubleAndReturnArgs: Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
+//doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
+//doubleAndReturnArgs([2],10,4) // [2, 20, 8]
+
+const doubleAndReturnArgs = (arr, ...args) => [
+  ...arr,
+  ...args.map((n) => n * 2),
+];
