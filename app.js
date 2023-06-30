0a8066ebf6a5a1dc765245702eacafb4ab76f080
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-06-30 13:18:46 -0400

    wrote mergeObjects func

--- app.js
+++ app.js
@@ -14,4 +14,6 @@ const filterOutOdds = (...nums) => nums.filter((num) => num % 2 === 0);
 // findMin(1,-1) // -1
 // findMin(3,1) // 1
 const findMin = (...args) => Math.min(...args);
-//3️⃣
+//3️⃣mergeObjects: Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+//mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
+const mergeObjects = (obj1, obj2) => ({ ...obj1, ...obj2 });
