f504fffc9fb7fd3fa0e9e1ccc25b2df072de030b
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-06-30 13:13:36 -0400

    wrote findMin func

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//refactor 3s5 code into es2015
+//refactor es5 code into es2015
+//ES5 Function that takes a variable number of arguments
 //1️⃣ Refactor it to use the rest operator & an arrow function:
 // function filterOutOdds() {
 //   var nums = Array.prototype.slice.call(arguments);
@@ -7,3 +8,10 @@
 //   });
 // }
 const filterOutOdds = (...nums) => nums.filter((num) => num % 2 === 0);
+
+//2️⃣findMin:Write a function called findMin that accepts a variable number of arguments and returns the smallest argument. Make sure to do this using the rest and spread operator.
+// findMin(1,4,12,-3) // -3
+// findMin(1,-1) // -1
+// findMin(3,1) // 1
+const findMin = (...args) => Math.min(...args);
+//3️⃣
